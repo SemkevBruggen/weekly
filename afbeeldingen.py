cb6f832281cb0ce980722a84d59950a36803831c
SemkevBruggen <semkevanbruggen@canvascompany.nl>
2025-03-25 15:17:05 +0100

    Update afbeeldingen.py

--- afbeeldingen.py
+++ afbeeldingen.py
@@ -13,7 +13,7 @@ from PIL import Image, UnidentifiedImageError
 
 # ✅ Webhooks
 URLS_WEBHOOK = "https://script.google.com/macros/s/AKfycbxHw1J2asNBEdd5LHZj2LqTjwKVsjKufYhMSSeq6nRhY65mTVeuDai_oSt_lWRB_MkE/exec"
-GOOGLE_SHEET_WEBHOOK = "https://script.google.com/macros/s/AKfycbx4UgIpTGFmFaEsxsWN8pJdNaqkhrMVGzPTKmsf18SfXPdWSjDv00jlM800uZKwDJsR/exec"
+GOOGLE_SHEET_WEBHOOK = "https://script.google.com/macros/s/AKfycbzlpZQHlOl-DcDclWEnVWl28PGfqvGPzeXDOFht_GO_U-3k_vHlh-0VB09mB9QEcvc/exec"
 
 # Instellingen
 STANDARD_SIZE = (256, 256)
